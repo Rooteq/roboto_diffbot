299e8fa5b6474ca5ab66769206400dc470e70789
Rooteq <szymerut@gmail.com>
2024-10-08 10:17:22 +0200

    publishing robot pose as twist

--- src/pose_publisher.cpp
+++ src/pose_publisher.cpp
@@ -4,7 +4,8 @@
 #include <string>
 
 #include "geometry_msgs/msg/transform_stamped.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
+// #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "tf2/exceptions.h"
 #include "tf2_ros/transform_listener.h"
@@ -24,7 +25,7 @@ public:
         tfBuffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
         tfListener = std::make_shared<tf2_ros::TransformListener>(*tfBuffer);
 
-        publisher = this->create_publisher<geometry_msgs::msg::PoseStamped>("diffbot_pose", 1);
+        publisher = this->create_publisher<geometry_msgs::msg::TwistStamped>("diffbot_pose", 1);
 
         timer = this->create_wall_timer(
             0.5s, std::bind(&PoseListener::onTimer, this));
@@ -44,22 +45,39 @@ private:
           return;
         }
 
-        geometry_msgs::msg::PoseStamped pose;
+        // geometry_msgs::msg::PoseStamped pose;
 
-        pose.header.stamp = this->get_clock()->now();
-        pose.header.frame_id = baseFrame;
+        geometry_msgs::msg::TwistStamped twist;
 
-        pose.pose.position.x = t.transform.translation.x;
-        pose.pose.position.y = t.transform.translation.y;
-        pose.pose.position.z = t.transform.translation.z;
-
-        pose.pose.orientation.w = t.transform.rotation.w;
-        pose.pose.orientation.x = t.transform.rotation.x;
-        pose.pose.orientation.y = t.transform.rotation.y;
-        pose.pose.orientation.z = t.transform.rotation.z;
+        twist.header.stamp = this->get_clock()->now();
+        twist.header.frame_id = baseFrame;
 
 
-        publisher->publish(pose);
+        twist.twist.linear.x = t.transform.translation.x;
+        twist.twist.linear.y = t.transform.translation.y;
+        twist.twist.linear.z = t.transform.translation.z;
+
+        double x = t.transform.rotation.x;
+        double y = t.transform.rotation.y;
+        double z = t.transform.rotation.z;
+        double w = t.transform.rotation.w;
+
+        twist.twist.angular.z = atan2(2.0f * (w * z + x * y), 1.0f - 2.0f * (y * y + z * z));
+
+        // pose.header.stamp = this->get_clock()->now();
+        // pose.header.frame_id = baseFrame;
+
+        // pose.pose.position.x = t.transform.translation.x;
+        // pose.pose.position.y = t.transform.translation.y;
+        // pose.pose.position.z = t.transform.translation.z;
+
+        // pose.pose.orientation.w = t.transform.rotation.w;
+        // pose.pose.orientation.x = t.transform.rotation.x;
+        // pose.pose.orientation.y = t.transform.rotation.y;
+        // pose.pose.orientation.z = t.transform.rotation.z;
+
+
+        publisher->publish(twist);
 
     }
 
@@ -68,7 +86,7 @@ private:
     std::string targetFrame;
     std::string baseFrame;
 
-    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher{nullptr};
+    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr publisher{nullptr};
 
     rclcpp::TimerBase::SharedPtr timer{nullptr};
 };
