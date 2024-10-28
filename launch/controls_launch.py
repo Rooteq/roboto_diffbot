6fcce2e98bda08e89e94546795fa09010e711be7
Rooteq <szymerut@gmail.com>
2024-10-28 20:26:37 +0100

    twist mux bug fix

--- launch/controls_launch.py
+++ launch/controls_launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
     twist_mux = Node(
         package='twist_mux',
         executable='twist_mux',
-        parameters=[twist_mux_params, {'use_sim_time': use_sim_time}],
+        parameters=[twist_mux_params, {'use_sim_time': use_sim_time}, {'use_stamped' : False}],
         remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
     )
     return LaunchDescription([
