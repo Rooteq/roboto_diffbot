f2297d8a0747e355bb3dadcf624e318125a77689
Rooteq <szymerut@gmail.com>
2024-12-09 13:05:10 +0100

    changed helper script

--- helper_scripts/ros2_save_map.sh
+++ helper_scripts/ros2_save_map.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-# Define the directory to save the map
 MAP_DIR=~/ros2_ws/src/roboto_diffbot/sim/map
-
-# Define the map name
 MAP_NAME=my_map
-
-# Create the directory if it doesn't exist
 mkdir -p "$MAP_DIR"
-
-# Save the map using the nav2_map_server command
 ros2 run nav2_map_server map_saver_cli -f "$MAP_DIR/$MAP_NAME" --ros-args -p map_subscribe_transient_local:=true
 
-# Check if the command was successful (based on $? exit code)
 if [ $? -eq 0 ]; then
-    echo "Map saved successfully at $MAP_DIR/$MAP_NAME"
+    echo "Saved map at $MAP_DIR/$MAP_NAME"
 else
-    echo "Failed to save the map."
+    echo "Error while saving map"
 fi
